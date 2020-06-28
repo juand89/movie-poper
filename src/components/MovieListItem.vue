ef99636c5066e40760bd923e5298bf435391703e
juan diego ureña vazquez <juandu89@gmail.com>
2020-06-28 16:56:13 +0900

    add hover effect for movie cards

--- src/components/MovieListItem.vue
+++ src/components/MovieListItem.vue
@@ -1,25 +1,33 @@
 <template>
   <v-flex md3 sm4 lg3 xl2 xs6>
-    <v-card color="indigo lighten-5" class="ma-2">
-      <v-img
-        :lazy-src="require('../assets/blur-img.png')"
-        transition="fade"
-        :aspect-ratio="0.7"
-        @click="onMovieSelect"
-        :src="getMoviePicture"
-        gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-        class="white--text align-end items-center"
-      >
-        <v-card-title v-text="movie.title"></v-card-title>
-      </v-img>
-      <v-card-actions>
-        <span>{{ getFomatDate }}</span>
-        <v-spacer></v-spacer>
-        <span class="pr-1">{{ movie.vote_average }}</span>
-        <v-icon color="yellow darken-2">mdi-star-circle</v-icon>
-        <FavoriteIcon :movie="movie" />
-      </v-card-actions>
-    </v-card>
+    <v-hover>
+      <template v-slot="{ hover }">
+        <v-card
+          :elevation="hover ? 16 : 3"
+          color="indigo lighten-5"
+          class="ma-2"
+        >
+          <v-img
+            :lazy-src="require('../assets/blur-img.png')"
+            transition="fade"
+            :aspect-ratio="0.7"
+            @click="onMovieSelect"
+            :src="getMoviePicture"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            class="white--text align-end items-center"
+          >
+            <v-card-title v-text="movie.title"></v-card-title>
+          </v-img>
+          <v-card-actions>
+            <span>{{ getFomatDate }}</span>
+            <v-spacer></v-spacer>
+            <span class="pr-1">{{ movie.vote_average }}</span>
+            <v-icon color="yellow darken-2">mdi-star-circle</v-icon>
+            <FavoriteIcon :movie="movie" />
+          </v-card-actions>
+        </v-card>
+      </template>
+    </v-hover>
   </v-flex>
 </template>
 <script>
@@ -61,11 +69,14 @@ export default {
       }
     },
     onMovieSelect() {
-      if(this.$store.state.movie.id && this.$store.state.movie.id !== this.movie.id) {
+      if (
+        this.$store.state.movie.id &&
+        this.$store.state.movie.id !== this.movie.id
+      ) {
         this.$store.commit('resetMovie')
       }
       this.$router.push(`/movie/${this.movie.id}`)
-    }
+    },
   },
 }
 </script>
